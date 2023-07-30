02329e1ed428953ee39dc36ee900c8d8db5b04e5
victor odambatafwa <omondivictoras@gmail.com>
2023-07-30 18:15:38 +0300

    Completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div id="#about">
+  return <div id="about">
 
   <h2>About Me</h2>
   <p>I am A full stack developer</p>
